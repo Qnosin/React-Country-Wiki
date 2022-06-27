2084cb3a5f981f1d4219699fa08ae8bfbb341889
jakub putaj <putaj@DESKTOP-B7VMT8C.localdomain>
2022-06-27 22:25:35 +0200

    added animation

--- src/components/AppHead.jsx
+++ src/components/AppHead.jsx
@@ -3,33 +3,34 @@ import { useEffect,useState } from 'react'
 import axios from 'axios';
 import { useContext } from 'react';
 import { CountryContext } from '../helper/CountryContext';
-import ReactPaginate from 'react-paginate';
+import AOS from "aos";
+import "aos/dist/aos.css";
 function AppHead({className}) {
     const [country,setCountry] = useState([]);
     const {isAll,isFilter,isSearched} = useContext(CountryContext);
     const {filterValue} = useContext(CountryContext);
     let tab = []
     useEffect(()=>{
+            AOS.init();
+            AOS.refresh();
             axios.get(`https://restcountries.com/v3.1/all`).then((data)=>{
-                        tab.push(data.data[0],data.data[1],data.data[4],data.data[5],data.data[6],data.data[11],data.data[15],data.data[20],data.data[26],data.data[9],data.data[13]);
+                        tab.push(data.data[0],data.data[2],data.data[10],data.data[4],data.data[6],data.data[10],data.data[15],data.data[20],data.data[26],data.data[9],data.data[12],data.data[20],data.data[21],data.data[22],data.data[23],data.data[24]);
                         setCountry(tab);
             })
     },[])
-
-
   return (
     <div className={className}>
         {country.map((data,index)=>{
             return(
-                <section key={index}>
-                    <img src={data.flags.svg}></img>
-                    <div>
-                        <h1>{data.name.common}</h1>
-                        <p>Population: {data.population}</p>
-                        <p>Region: {data.continents[0]}</p>
-                        <p>Capital: {data.capital}</p>
-                    </div>
-                </section>
+                <section data-aos="fade-up" key={index}>
+                         <img src={data.flags.svg}></img>
+                         <div>
+                             <h1>{data.name.common}</h1>
+                             <p>Population: {data.population}</p>
+                             <p>Region: {data.continents[0]}</p>
+                             <p>Capital: {data.capital}</p>
+                         </div>
+                     </section>
             )
         })}
     </div>
